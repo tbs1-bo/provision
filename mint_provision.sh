0faf8f6db69d98915b805642b54ea5e660442547
Marco Bakera <marco@bakera.de>
2024-11-24 15:50:33 +0100

    prov mint

---
+++ mint_provision.sh
@@ -0,0 +1,41 @@
+# Mit diesem Skript lässt sich ein Linux Mint mit den notwendigen Programmen
+# und Bibliotheken einrichten.
+#
+# Zusätzlich wird ein LTSP-Server eingerichtet, der das fertige Image über 
+# Netzboot verteilt.
+
+ADMIN_USER=admin
+
+echo "# update package sources"
+sudo apt update
+
+echo "# enable snap"
+sudo mv /etc/apt/preferences.d/nosnap.pref ~/Dokumente/nosnap.backup
+sudo apt install snapd
+
+echo "# install mqtt explorer"
+snap install mqtt-explorer
+
+echo "# install vscode"
+snap install code --classic
+
+echo "# install mosquitto, paho-mqtt, arduino"
+sudo apt install mosquitto python3-paho-mqtt arduino
+
+echo "# create a file /etc/mosquitto/conf.d/allow_anon.conf"
+echo "# add the following lines:"
+echo "allow_anonymous true"
+echo "listener 1883 0.0.0.0"
+echo "restart broker: systemctl restart mosquitto"
+
+echo "# install ltsp"
+sudo apt install --install-recommends ltsp ipxe dnsmasq nfs-kernel-server openssh-server squashfs-tools ethtool net-tools epoptes
+sudo gpasswd -a $ADMIN_USER epoptes
+
+echo "# provision LTSP"
+sudo ltsp dnsmasq
+sudo ltsp image /
+sudo ltsp ipxe
+sudo ltsp nfs
+sudo ltsp initrd
+
