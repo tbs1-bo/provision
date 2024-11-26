5ea43ac8298652dd50b419cd331ac1ee251a14c3
Marco Bakera <marco@bakera.de>
2024-11-26 18:09:25 +0100

    mqtt explorer: using appimage instead of snap

--- mqtt.sh
+++ mqtt.sh
@@ -7,7 +7,12 @@ sudo mv /etc/apt/preferences.d/nosnap.pref ~/Dokumente/nosnap.backup
 sudo apt install snapd
 
 echo "# install mqtt explorer"
-sudo snap install mqtt-explorer
+# sudo snap install mqtt-explorer
+# snap installation currently not working: 
+# https://github.com/thomasnordquist/MQTT-Explorer/issues/611
+
+wget https://github.com/thomasnordquist/MQTT-Explorer/releases/download/v0.4.0-beta.6/MQTT-Explorer-0.4.0-beta.6.AppImage -O ~/Schreibtisch/MQTT-Explorer.AppImage
+chmod +x ~/Schreibtisch/MQTT-Explorer.AppImage
 
 echo "# install mosquitto, paho-mqtt"
 sudo apt install mosquitto python3-paho-mqtt
