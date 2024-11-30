a51a99dc8a3a9a3a515f52aeae6d018dc417d0a4
Marco Bakera <marco@bakera.de>
2024-11-30 14:53:35 +0100

    add local llm

---
+++ local_llm.sh
@@ -0,0 +1,27 @@
+# default port 11434 sometimes not working
+OLLAMA_PORT=11435
+OLLAMA_HOST="127.0.0.1:$OLLAMA_PORT"
+
+echo "install ollama"
+curl -fsSL https://ollama.com/install.sh | sh 
+
+echo "test run ollama"
+ollama run llama3.2
+
+echo "installing docker"
+sudo apt install docker.io 
+
+echo "addind current user to docker group"
+sudo usermod -a -G docker $USER 
+
+echo "starting docker container"
+docker run -d --network=host \
+  -v open-webui:/app/backend/data \
+  -e OLLAMA_BASE_URL=http://$OLLAMA_HOST \
+  --name open-webui \
+  --restart always \
+  ghcr.io/open-webui/open-webui:main
+
+echo "starting ollama server respecting ollama host config $OLLAMA_HOST"
+ollama serve 
+
